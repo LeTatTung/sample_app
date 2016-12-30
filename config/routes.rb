aa16ee883e6e9c11965819ff6beafbfd813f6cdc
Le Tat Tung <letattungtb@gmail.com>
2016-12-30 10:16:27 +0700

    Finish user edit, update, index, and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
 
-  resources :users, only: [:create, :show, :destroy]
+  resources :users, except: [:show, :destroy]
 end
